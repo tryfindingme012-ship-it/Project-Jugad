39684f5f7ca72058368a70a17f43eeb8cd9cee0c
tryfindingme012-ship-it <tryfindingme012@gmail.com>
2026-03-27 13:16:31 +0530

    Add DocumentViewer component for viewing documents and applying digital signatures

---
+++ DocumentViewer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DocumentViewer = () => {
+    return (
+        <div>
+            <h1>Document Viewer</h1>
+            <p>This component will handle viewing documents and applying digital signatures.</p>
+        </div>
+    );
+};
+
+export default DocumentViewer;
